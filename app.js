b2a733cc7c4ab97a5f0e5be8b3ad80e803442595
Daniel Noon <supercookies@outlook.com>
2018-05-31 14:44:28 -0500

    socket.io redis (unfinished)

--- app.js
+++ app.js
@@ -3,18 +3,12 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   path: '/'
 });
-const PubSub = require('@google-cloud/pubsub');
-
-const projectId = 'seminar-205000';
-let debugUser = '';
-
-const pubsubClient = new PubSub({
-  projectId
-});
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
 
 io.on('connection', socket => {
   socket.on('join', (server, ack) => {
-    socket.join(server, err => {
+    socket.join("test", err => {
       console.log("A SOCKET JOINED ", server);
       console.log("Socket's rooms: ", socket.rooms);
       debugUser = socket.id;
@@ -22,18 +16,6 @@ io.on('connection', socket => {
     });
   })
 });
-
-pubsubClient
-  .topic('new-message')
-  .subscription('new-message')
-  .on('message', data => {
-    const message = JSON.parse(data.data.toString());
-    console.log("A MESSAGE WAS SENT TO ", message.server);
-    console.log(io.sockets.sockets[debugUser].rooms);
-    // io.emit('new-message', message);
-    io.sockets.to(message.server).emit('new-message', message);
-    data.ack();
-  });
 
 app.get('/test', function (req, res) {
   res.send('<h1>Hello world</h1>');
