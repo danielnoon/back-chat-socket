5e9da0ed416c336244a4934ba446560e94a3e542
Daniel Noon <supercookies@outlook.com>
2018-05-30 23:22:47 -0500

    what the hell

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
-  path: '/',
-  origins: '*:*'
+  path: '/'
 });
 const PubSub = require('@google-cloud/pubsub');
 
